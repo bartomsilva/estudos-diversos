69040f8123a422a60c4caad2a30be87ac97e9ec5
bartomsilva <bartomsilva@gmail.com>
2023-03-13 06:52:58 -0300

    exercicios

---
+++ CFB/Java/Alomundo/Alomundo.java
@@ -0,0 +1,7 @@
+public class Alomundo{
+    public static void main(String[] args){
+        System.out.print("Alo mundo!");
+
+    }
+
+}
---
+++ CFB/Java/Alomundo/CursoJava.java
@@ -0,0 +1,12 @@
+public class CursoJava {
+    public static void main(String[] agrs){
+
+        int num=0;
+        String name="Bart";     
+        
+        System.out.println("Curso de Java");
+        System.out.printf("Nome: %s%nAno: %d%n", "Bart", 2023);
+        System.out.printf("Valor da variavel num: %d%n",num);
+        System.out.printf("Valor da variavel name: %s%n",name);
+    }    
+}
