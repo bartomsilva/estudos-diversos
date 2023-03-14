87bc111d2bbb1d38406501c06f2541a08c296654
bartomsilva <bartomsilva@gmail.com>
2023-03-14 08:01:54 -0300

    exercicios

---
+++ ese.js
@@ -0,0 +1,17 @@
+/*
+O que são os operadores relacionais?
+> que representa maior que.
+< que representa menor que.
+>= que representa maior ou igual.
+<= que representa menor ou igual.
+== que representa igual.
+!= que representa diferente.
+=== que representa estritamente igual.
+!== que representa estritamente diferente.
+*/
+
+valorA = 100
+valorB = "100"
+console.log( valorA == valorB )
+
+
